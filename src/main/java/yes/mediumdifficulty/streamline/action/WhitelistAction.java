b47703db8307070eae194e2b53347ff3cbd1bbe0
Hexy32 <sunshine.goody8762@gmail.com>
2023-07-14 08:54:36 -0400

    Bug fixes and logs

--- src/main/java/yes/mediumdifficulty/streamline/action/WhitelistAction.java
+++ src/main/java/yes/mediumdifficulty/streamline/action/WhitelistAction.java
@@ -3,8 +3,10 @@ package yes.mediumdifficulty.streamline.action;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.Server;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.json.JSONObject;
 
+import yes.mediumdifficulty.streamline.Streamline;
 import yes.mediumdifficulty.streamline.utils.Uuid;
 
 import java.util.UUID;
@@ -12,6 +14,8 @@ import java.util.UUID;
 public class WhitelistAction extends AbstractAction {
     @Override
     public String handle(String payload) {
+        JavaPlugin plugin = JavaPlugin.getPlugin(Streamline.class);
+
         JSONObject payloadJson = new JSONObject(payload);
 
         String operation = payloadJson.getString("operation");
@@ -21,13 +25,16 @@ public class WhitelistAction extends AbstractAction {
         
         switch (operation) {
             case "add" -> {
-                String fullUuid = Uuid.trimmedToFull(payloadJson.getString("uuid"));
+                plugin.getLogger().info(payloadJson.toString());
+                String uuid = payloadJson.getString("uuid");
+                String fullUuid = Uuid.trimmedToFull(uuid);
                 
                 server.getOfflinePlayer(UUID.fromString(fullUuid)).setWhitelisted(true);
                 return payload;
             }
             case "remove" -> {
-                String fullUuid = Uuid.trimmedToFull(payloadJson.getString("uuid"));
+                String uuid = payloadJson.getString("uuid");
+                String fullUuid = Uuid.trimmedToFull(uuid);
                 
                 server.getOfflinePlayer(UUID.fromString(fullUuid)).setWhitelisted(false);
                 return payload;
